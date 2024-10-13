038923c11d6b9048719f620ec28a949c5bcc34e3
MUHAMMDA RASHID <m.rashid212823@gmail.com>
2024-10-13 21:12:34 +0500

    good

--- crud-firebase/src/app/page.tsx
+++ crud-firebase/src/app/page.tsx
@@ -123,6 +123,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
+      <h1>Welcome</h1>
       <label htmlFor="todo" className="mb-2 text-lg font-medium">
         What&apos;s Todo
       </label>
